6c23ddc655d85bee6e083d46e1f04b8e452834d7
Tip24 <tip24.developer@gmail.com>
2020-01-24 19:49:24 +0400

    Updates

---
+++ precache-manifest.5ddd2425384f58a8562d5f78d2967bb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dd400be03a2c5623831636163721d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98e30b89f114f2a11aa",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd562c419bebf2de7868",
+    "url": "/static/js/2.c04e0875.chunk.js"
+  },
+  {
+    "revision": "485c9edbd633fb8f5cdac9c53165b247",
+    "url": "/static/js/2.c04e0875.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d98e30b89f114f2a11aa",
+    "url": "/static/js/main.1e1957f3.chunk.js"
+  },
+  {
+    "revision": "39b17d913d07f3b8207e",
+    "url": "/static/js/runtime-main.993019f4.js"
+  }
+]);
