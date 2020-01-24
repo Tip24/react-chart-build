bd6a158c56712542b635ce9db44e6b87cc0b3c68
Tip24 <56535193+Tip24@users.noreply.github.com>
2020-01-24 20:38:11 +0400

    Add files via upload

---
+++ precache-manifest.6049c6e8da29d02f01455fe58375734b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0eeb6934060a0a4d23323001959630",
+    "url": "/react-chart-build/index.html"
+  },
+  {
+    "revision": "149e0636879bd538be9f",
+    "url": "/react-chart-build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5402cbe50e43807fe427",
+    "url": "/react-chart-build/static/js/2.ff23286a.chunk.js"
+  },
+  {
+    "revision": "485c9edbd633fb8f5cdac9c53165b247",
+    "url": "/react-chart-build/static/js/2.ff23286a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "149e0636879bd538be9f",
+    "url": "/react-chart-build/static/js/main.e6863d3e.chunk.js"
+  },
+  {
+    "revision": "92e5c54f338960eaa9e8",
+    "url": "/react-chart-build/static/js/runtime-main.9a168e0f.js"
+  }
+]);
